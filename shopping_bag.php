b20946d5a1ee41be6a3e24199f2f5458d0f39175
Jellyyyyyyy <jerylloi019@gmail.com>
2022-11-18 01:59:48 +0800

    shopping cart page

--- shopping_bag.php
+++ shopping_bag.php
@@ -7,13 +7,67 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title></title>
   <?php include_once "include/head.inc.php" ?>
-  <link rel="stylesheet" href="css/.css">
-  <script src="js/.js" defer></script>
+  <link rel="stylesheet" href="css/shopping_bag.css">
+  <!-- <script src="js/.js" defer></script> -->
 </head>
 
 <body>
   <?php include_once "include/nav.inc.php" ?>
   <main>
+    <div class="fluid-container mb-4 h-100 w-100 cart-container">
+      <div class="title-text">
+        <h1>Your bag total is $3000</h1>
+        <button class="btn btn-dark mt-2">Check out</button>
+      </div>
+      <div class="items">
+        <div class="item">
+          <img src="images/login_page_photo.jpeg" alt="">
+          <div class="item-details">
+            <div class="item-name">
+              <span>BAG OF AWESOMENESS</span>
+            </div>
+            <div class="item-quantity">
+              <i class='bx bx-sm bx-tada-hover bx-minus-circle'></i>
+              1
+              <i class='bx bx-sm bx-tada-hover bx-plus-circle'></i>
+            </div>
+            <div class="item-price">S$100</div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="images/login_page_photo.jpeg" alt="">
+          <div class="item-details">
+            <div class="item-name">BAG</div>
+            <div class="item-quantity">
+              <i class='bx bx-sm bx-tada-hover bx-minus-circle'></i>
+              1
+              <i class='bx bx-sm bx-tada-hover bx-plus-circle'></i>
+            </div>
+            <div class="item-price">S$100</div>
+          </div>
+        </div>
+      </div>
+      <div class="price-container">
+        <div class="price-content">
+          <div class="price-subcontent">
+            <span>Subtotal</span>
+            <span>S$100</span>
+          </div>
+          <div class="price-subcontent">
+            <span>Shipping</span>
+            <span>S$10</span>
+          </div>
+          <div class="price-subcontent total">
+            <span>Your Total</span>
+            <span>$110</span>
+          </div>
+          <span class="price-info">Includes GST of $0</span>
+        </div>
+      </div>
+      <div class="checkout-btn">
+        <button class="btn btn-dark mt-2">Check out</button>
+      </div>
+    </div>
   </main>
   <?php include_once "include/footer.inc.php" ?>
 </body>
