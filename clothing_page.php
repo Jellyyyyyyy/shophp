893074583957eb43d24604ab79c6674f96c8304c
spookyjos <josiahrachmat2@gmail.com>
2022-11-18 10:50:41 +0800

    someting

--- clothing_page.php
+++ clothing_page.php
@@ -25,8 +25,44 @@
             </div>
         </div>
     </section>
-    <div class="container" style="height: 10em;">
+    <div class="main-container">
+        <div class="grid-container">
+            <template class="card-template">
+                <div class="card col-md-4">
+                    <div class="arrow-previous arrow">
+                        <i class='bx bxs-left-arrow'></i>
+                    </div>
+                    <div class="imageDIV">
+                        <img data-item-image>
+                    </div>
+                    <div class="text-container">
+                        <div class="category-container">
+                            <span data-item-category>UNISEX</span>
+                            <span data-item-size>XS-XL</span>
+                        </div>
+                        <span data-item-name>Pocketable Coat</span>
+                        <span data-item-price>$129.90</span>
+                    </div>
+                    <div class="arrow-next arrow">
+                        <i class='bx bxs-right-arrow'></i>
+                    </div>
+                </div>
+            </template>
+        </div>
     </div>
+    <script>
+    <?php
+        include_once "include/functions.inc.php";
+        foreach (getItems("clothing") as $item) {
+        foreach ($item as $key => $value) {
+            if (strpos($value, "'") && $key !== "image") $item[$key] = str_replace("'", "&#39;", $item[$key]);
+            if (strpos($value, '"') && $key !== "image") $item[$key] = str_replace('"', "&#34;", $item[$key]);
+            if (strpos($value, ')') && $key !== "image") $item[$key] = str_replace(')', "&#41;", $item[$key]);
+        }
+        echo "addCard('{$item["image"]}', '{$item["category"]}', '{$item["stock"]}', '{$item["name"]}', '{$item["price"]}', '.grid-container');";
+     }
+     ?> 
+    </script>
     <?php
         include_once "include/footer.inc.php"
     ?>
