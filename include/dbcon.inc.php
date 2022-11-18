0d16ce253d1e6157f3761f9ec006ec50c54c0972
Jellyyyyyyy <jerylloi019@gmail.com>
2022-11-19 00:43:01 +0800

    fixed footer

--- include/dbcon.inc.php
+++ include/dbcon.inc.php
@@ -1,8 +1,11 @@
 <?php
-$config = parse_ini_file('../private/db-config.ini');
-$conn = new mysqli(
-  $config['servername'],
-  $config['username'],
-  $config['password'],
-  $config['dbname']
-);
+if (!isset($conn)) {
+  global $conn;
+  $config = parse_ini_file('../private/db-config.ini');
+  $conn = new mysqli(
+    $config['servername'],
+    $config['username'],
+    $config['password'],
+    $config['dbname']
+  );
+}
