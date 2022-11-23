9cfbf2870c592e35ff4b046ea8ee3ff021073653
spookyjos <josiahrachmat2@gmail.com>
2022-11-23 21:53:56 +0800

    abuabuabuabfuabuabubafub

--- shopping_bag.php
+++ shopping_bag.php
@@ -18,66 +18,124 @@
       <div class="jumbotron" id="displayJumbo">
         <div class="row">
           <div class="col-md-4">
-            <h1 class="hugeText">CHECKOUT</h1>
+            <h1 class="hugeText">CART</h1>
             <p class="paraText">Thank you for shopping with us! We hope you come back again!</p>
           </div>
         </div>
       </div>
     </section>
-    <div class="fluid-container mb-4 h-100 w-100 cart-container">
-      <!--<div class="title-text">
-        <h1>Your bag total is $3000</h1>
-        <button class="btn btn-dark mt-2">Check out</button>
-      </div>-->
-      <div class="items">
-        <div class="item">
-          <img src="images/login_page_photo.jpeg" alt="">
-          <div class="item-details">
-            <div class="item-name">
-              <span>BAG OF AWESOMENESS</span>
+     <!-- <div class="fluid-container mb-4 h-100 w-100 cart-container">
+          
+          <div class="items">
+            <div class="item">
+              <img src="images/login_page_photo.jpeg" alt="">
+              <div class="item-details">
+                <div class="item-name">
+                  <span>BAG OF AWESOMENESS</span>
+                </div>
+                <div class="item-quantity">
+                  <i class='bx bx-sm bx-tada-hover bx-minus-circle'></i>
+                  1
+                  <i class='bx bx-sm bx-tada-hover bx-plus-circle'></i>
+                </div>
+                <div class="item-price">S$100</div>
+              </div>
             </div>
-            <div class="item-quantity">
-              <i class='bx bx-sm bx-tada-hover bx-minus-circle'></i>
-              1
-              <i class='bx bx-sm bx-tada-hover bx-plus-circle'></i>
+            <div class="item">
+              <img src="images/login_page_photo.jpeg" alt="">
+              <div class="item-details">
+                <div class="item-name">BAG</div>
+                <div class="item-quantity">
+                  <i class='bx bx-sm bx-tada-hover bx-minus-circle'></i>
+                  1
+                  <i class='bx bx-sm bx-tada-hover bx-plus-circle'></i>
+                </div>
+                <div class="item-price">S$100</div>
+              </div>
             </div>
-            <div class="item-price">S$100</div>
           </div>
-        </div>
-        <div class="item">
-          <img src="images/login_page_photo.jpeg" alt="">
-          <div class="item-details">
-            <div class="item-name">BAG</div>
-            <div class="item-quantity">
-              <i class='bx bx-sm bx-tada-hover bx-minus-circle'></i>
-              1
-              <i class='bx bx-sm bx-tada-hover bx-plus-circle'></i>
+          <div class="price-container">
+            <div class="price-content">
+              <div class="price-subcontent">
+                <span>Subtotal</span>
+                <span>S$100</span>
+              </div>
+              <div class="price-subcontent">
+                <span>Shipping</span>
+                <span>S$10</span>
+              </div>
+              <div class="price-subcontent total">
+                <span>Your Total</span>
+                <span>$110</span>
+              </div>
+              <span class="price-info">Includes GST of $0</span>
             </div>
-            <div class="item-price">S$100</div>
+          </div>
+          <div class="checkout-btn">
+            <button class="btn btn-dark mt-2">Check out</button>
+          </div>
+      </div> -->
+      <!-- Testing segment comment out later -->
+      <div class="fluid-container">
+        <div class="row">
+          <div class="col-md-6 cart-container">
+            <div class="items">
+              <div class="item">
+                <div class="imageDiv">
+                  <img src="images/login_page_photo.jpeg" alt="">
+                </div>
+                <div class="item-details">
+                  <div class="item-name">
+                    <span>BAG OF AWESOMENESS</span>
+                  </div>
+                  <div class="item-quantity">
+                    <i class='bx bx-sm bx-tada-hover bx-minus-circle'></i>
+                    1
+                    <i class='bx bx-sm bx-tada-hover bx-plus-circle'></i>
+                  </div>
+                  <div class="item-price">S$100</div>
+                </div>
+              </div>
+              <div class="item">
+                <div class="imageDiv">
+                  <img src="images/login_page_photo.jpeg" alt="">
+                </div>
+                <div class="item-details">
+                  <div class="item-name">BAG</div>
+                  <div class="item-quantity">
+                    <i class='bx bx-sm bx-tada-hover bx-minus-circle'></i>
+                    1
+                    <i class='bx bx-sm bx-tada-hover bx-plus-circle'></i>
+                  </div>
+                  <div class="item-price">S$100</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 cart-container">
+            <div class="price-container">
+              <div class="price-content">
+                <div class="price-subcontent">
+                  <span>Subtotal</span>
+                  <span>S$100</span>
+                </div>
+                <div class="price-subcontent">
+                  <span>Shipping</span>
+                  <span>S$10</span>
+                </div>
+                <div class="price-subcontent total">
+                  <span>Your Total</span>
+                  <span>$110</span>
+                </div>
+                <span class="price-info">Includes GST of $0</span>
+              </div>
+            </div>
+            <div class="checkout-btn">
+              <button class="btn btn-dark mt-2">Check out</button>
+            </div>
           </div>
         </div>
       </div>
-      <div class="price-container">
-        <div class="price-content">
-          <div class="price-subcontent">
-            <span>Subtotal</span>
-            <span>S$100</span>
-          </div>
-          <div class="price-subcontent">
-            <span>Shipping</span>
-            <span>S$10</span>
-          </div>
-          <div class="price-subcontent total">
-            <span>Your Total</span>
-            <span>$110</span>
-          </div>
-          <span class="price-info">Includes GST of $0</span>
-        </div>
-      </div>
-      <div class="checkout-btn">
-        <button class="btn btn-dark mt-2">Check out</button>
-      </div>
-    </div>
   </main>
   <?php include_once "include/footer.inc.php" ?>
 </body>
