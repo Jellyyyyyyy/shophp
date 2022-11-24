eb71ac04da4caa197a61ed56730ca26eb0b74fd8
spookyjos <josiahrachmat2@gmail.com>
2022-11-24 23:09:41 +0800

    someting

--- shopping_bag.php
+++ shopping_bag.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title></title>
+  <title>CART</title>
   <?php include_once "include/head.inc.php" ?>
   <link rel="stylesheet" href="css/shopping_bag.css">
   <script type="text/javascript">
@@ -15,6 +15,12 @@
     V.init({
       apikey: "3V3OR5G8DJXTGQ4TQZYI21eZqnsETcg7z-49s08D7UMqfjrvE",
       encryptionKey: "BOOPY01VJDM2QI4M6B4P13kV-che-E8ZpBFvnM5iGXschQDrM",
+      settings: {
+        shipping: {
+          acceptedRegions: ["SG"],
+          collectShipping: "true",
+        },
+      },
       paymentRequest: {
         currencyCode: "SGD",
         subtotal: totalCost,
@@ -40,9 +46,10 @@
       <div class="jumbotron" id="displayJumbo">
         <div class="row">
           <div class="col-md-4">
-            <h1 class="hugeText">CHECKOUT</h1>
+            <h1 class="hugeText">CART</h1>
             <p class="paraText">Thank you for shopping with us! We hope you come back again!</p>
           </div>
+          <div class="col-md-8"></div>
         </div>
       </div>
     </section>
@@ -66,19 +73,31 @@
             <div class="item-price">S$100</div>
           </div>
         </div>
+
         <div class="item">
-          <img src="images/login_page_photo.jpeg" alt="">
-          <div class="item-details">
-            <div class="item-name">BAG</div>
-            <div class="item-quantity">
-              <i class='bx bx-sm bx-tada-hover bx-minus-circle'></i>
-              1
-              <i class='bx bx-sm bx-tada-hover bx-plus-circle'></i>
+          <div class="row">
+            <div class="col-md-5">
+              <div class="productImg">
+                <img src="images/login_page_photo.jpeg" alt="">
+              </div>  
             </div>
-            <div class="item-price">S$100</div>
-          </div>
+            <div class="col-md-7">
+              <div class="item-details">
+                <div class="item-name">BAG</div>
+                <div class="item-quantity">
+                  <i class='bx bx-sm bx-tada-hover bx-minus-circle'></i>
+                  1
+                  <i class='bx bx-sm bx-tada-hover bx-plus-circle'></i>
+                </div>
+                <div class="item-price">S$100</div>
+              </div>
+            </div>
         </div>
+
       </div>
+
+
+
       <div class="price-container">
         <div class="price-content">
           <div class="price-subcontent">
@@ -94,12 +113,15 @@
             <span>$110</span>
           </div>
           <span class="price-info">Includes GST of $0</span>
+          <div class="visaBtn">
+            <img alt="Visa Checkout" class="v-button" role="button"
+              src="https://sandbox.secure.checkout.visa.com/wallet-services-web/xo/button.png" />
+          </div>
         </div>
       </div>
-      <img alt="Visa Checkout" class="v-button" role="button"
-        src="https://sandbox.secure.checkout.visa.com/wallet-services-web/xo/button.png" height="500" width="1000" />
-      <script type="text/javascript"
-        src="https://sandbox-assets.secure.checkout.visa.com/checkout-widget/resources/js/integration/v1/sdk.js">
+      
+      <script type="text/javascript" src="https://sandbox-assets.secure.checkout.visa.com/checkout-widget/resources/js/integration/v1/sdk.js">
+
       </script>
 
     </div>
@@ -108,3 +130,4 @@
 </body>
 
 </html>
+
