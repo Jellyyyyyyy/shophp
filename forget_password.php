30fb139e7fbaddf93fd9a513c0cf92ae193f08bd
spookyjos <josiahrachmat2@gmail.com>
2022-11-25 02:53:28 +0800

    forgetpw

--- forget_password.php
+++ forget_password.php
@@ -20,8 +20,8 @@
           <div class="col-sm-6 text-black">
             <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
               <form action="process_forget" method="post" target="_self" style="width: 23rem">
-                <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px">
-                  Forget Password
+                <h3 class="fw-bold mb-3 pb-3" style="letter-spacing: 1px">
+                  Forgot Password
                 </h3>
                 <?php
                 if (isset($_GET['forgetsuccess'])) {
@@ -39,7 +39,7 @@
                   <label class="form-label login-email-label" for="forget_pwd">Email</label>
                 </div>
                 <div class="pt-1 mb-4">
-                  <button class="btn btn-info btn-lg btn-block submit-button" type="submit">
+                  <button class="btn btn-dark btn-lg btn-block submit-button" type="submit">
                     Send Email for Password Reset
                   </button>
                 </div>
@@ -47,6 +47,7 @@
             </div>
           </div>
           <div class="col-sm-6 px-0 d-none d-sm-block">
+            <div class="login-img-overlay"></div>
             <img src="images/login_page_photo.jpeg" alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left">
           </div>
         </div>
