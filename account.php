b354f905f9d1f634b70ab282deffb56b3acec402
spookyjos <josiahrachmat2@gmail.com>
2022-11-25 17:45:40 +0800

    accounts page mobile view

--- account.php
+++ account.php
@@ -27,48 +27,49 @@
                 </div>
 
 
-
-                <form action="process_account.php" method="post">
-                    <div class="form-group" style="padding: 7px 300px;">
-                        <label for="fname">First Name:</label>
-                        <input type="text" id="fname" maxlength="45" name="fname"
-                               placeholder="Enter new first name" class="form-control">
-                    </div>
-                    <div class="form-group" style="padding: 7px 300px;">
-                        <label for="lname">Last Name:</label>
-                        <input type="text" id="lname" required maxlength="45" name="lname"
-                               placeholder="Enter new last name" class="form-control">
-                    </div>
-                    <div class="form-group" style="padding: 7px 300px;">
-                        <label for="old_email">Current(Old) Email:</label>
-                        <input type="email" id="old_email" required name="email"
-                               placeholder="Enter old email" class="form-control">
-                    </div>
-                    <div class="form-group" style="padding: 7px 300px;">
-                        <label for="new_email">New Email:</label>
-                        <input type="email" id="new_email" required name="new_email"
-                               placeholder="Enter new email" class="form-control">
-                    </div>
-                    <div class="form-group" style="padding: 7px 300px;">
-                        <label for="pwd">Password:</label>
-                        <input type="password" id="pwd" required name="pwd"
-                               placeholder="Enter password" class="form-control">
-                    </div>
-                    <div class="form-group" style="padding: 7px 300px;">
-                        <label for="pwd_confirm">Confirm Password:</label>
-                        <input type="password" id="pwd_confirm" required name="pwd_confirm"
-                               placeholder="Confirm password" class="form-control">
-                    </div>
-                    <div style="padding: 7px 300px;">
-                        
-                        <input type="checkbox" required name="agree" class="form-check-input" style="scale: 1.5;" id="account_checkbox">
-                        <label for="account_checkbox">Agree to terms and conditions.</label>
-                        
-                    </div>
-                    <div style="padding: 7px 300px;">
-                        <button type="submit" class="btn btn-primary" style="color:white; background-color: black;" title="Title">Apply Changes</button>
-                    </div>
-                </form>
+                <div class="editDetailsForm">
+                    <form action="process_account.php" method="post">
+                        <div class="form-group" style="padding: 7px 300px;">
+                            <label for="fname">First Name:</label>
+                            <input type="text" id="fname" maxlength="45" name="fname"
+                                placeholder="Enter new first name" class="form-control">
+                        </div>
+                        <div class="form-group" style="padding: 7px 300px;">
+                            <label for="lname">Last Name:</label>
+                            <input type="text" id="lname" required maxlength="45" name="lname"
+                                placeholder="Enter new last name" class="form-control">
+                        </div>
+                        <div class="form-group" style="padding: 7px 300px;">
+                            <label for="old_email">Current(Old) Email:</label>
+                            <input type="email" id="old_email" required name="email"
+                                placeholder="Enter old email" class="form-control">
+                        </div>
+                        <div class="form-group" style="padding: 7px 300px;">
+                            <label for="new_email">New Email:</label>
+                            <input type="email" id="new_email" required name="new_email"
+                                placeholder="Enter new email" class="form-control">
+                        </div>
+                        <div class="form-group" style="padding: 7px 300px;">
+                            <label for="pwd">Password:</label>
+                            <input type="password" id="pwd" required name="pwd"
+                                placeholder="Enter password" class="form-control">
+                        </div>
+                        <div class="form-group" style="padding: 7px 300px;">
+                            <label for="pwd_confirm">Confirm Password:</label>
+                            <input type="password" id="pwd_confirm" required name="pwd_confirm"
+                                placeholder="Confirm password" class="form-control">
+                        </div>
+                        <div class="form-group2" style="padding: 7px 300px;">
+                            
+                            <input type="checkbox" required name="agree" class="form-check-input" style="scale: 1.5;" id="account_checkbox">
+                            <label for="account_checkbox">Agree to terms and conditions.</label>
+                            
+                        </div>
+                        <div class="form-group2" style="padding: 7px 300px;">
+                            <button type="submit" class="btn btn-primary" style="color:white; background-color: black;" title="Title">Apply Changes</button>
+                        </div>
+                    </form>
+                </div>
 
             </div>
         </main>
